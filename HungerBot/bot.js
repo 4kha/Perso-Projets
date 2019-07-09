8f1664f6a64ab3518b56544a8aba417597707ec2
Kha.sh <KiraShinin@gmail.com>
2019-07-09 21:37:49 +0200

    Correction of stuff

--- HungerBot/bot.js
+++ HungerBot/bot.js
@@ -1,16 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const HBfeature = require('./features/HBfeature.js')
+const HBfeature = require('./features/HBfeature.js');
+const key = require('./auth.json');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+let NillaHunt = 1;
+
 client.on('message', msg => {
+  let NillaHunt = 1;
   if (msg.content.startsWith("h!")) {
     let msga = client.channels.get(msg.channel.id);
     let command = msg.content.split(' ')[0].substring(2).toLowerCase();
     let argument = msg.content.substring(2 + command.length).trimLeft();
+
+    let NillaHunt = 1;
+
     console.log(msg.author.username + ": " + command);
 
     switch (command) {
@@ -28,27 +35,30 @@ client.on('message', msg => {
         break;
 
       case 'listfood' :
-        msg.reply("So we have: " + HBfeature.foodList());
+        argument = (argument === ''? 1 : argument);
+        msg.reply(`**page ${argument} -` + " Food list page:**\n" + HBfeature.foodList(argument));
         break;
       case 'addfood' :
-        if (HBfeature.addFood(argument))
-          msg.reply(argument + " have been added");
-        else
-          msg.reply("Usage: h!addfood [argument]");
+        msg.reply(HBfeature.addFood(argument));
         break;
       case 'delfood' :
-        if (HBfeature.delFood(argument))
-          msg.reply(argument + " have been removed");
-        else
-          msg.reply("Usage: h!delfood [argument]");
+        msg.reply(HBfeature.delFood(argument));
         break;
       case 'cook' :
           msg.reply(HBfeature.cookFood());
+          break;
+      case 'magicball' :
+          msg.reply(HBfeature.magicBall(argument));
+          break;
       }
     }
     else if (msg.content.toLowerCase() === "bot is gay")
       msg.reply("no u");
+//    if (msg.content.toLowerCase().indexOf("nilla") != -1 && NillaHunt === 1)
+//    { 
+//      msg.reply("She is married");
+//    NillaHunt = 0;}
 });
 
 
-client.login('NTgzMjkxMzUyMDA0MDM0NTYw.XO6dyg.Jvm6zCk9VJ0z4MM47YwNYiu-OSk');
+client.login(key.token);
